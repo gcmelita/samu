0aba92ba34c20876f481dcdc5e5dc135b1138129
gcmelita <gabrielmelita@gmail.com>
2017-07-10 21:20:20 -0300

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,22 @@ import {UFService} from './services/uf.service'
 import {Dados} from './types/samu';
 import {SamuService} from './services/samu.service'
 
+import {UFs} from './services/mock-ufs'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UFService, SamuService]
 })
 export class AppComponent implements OnInit {
     title = 'app';
     ufs : UF[];
+    uf : UF;
+    media: number;
+    dados: Dados[];
     dados_da_samu : Dados[];
+    id = 16;
 
     constructor(private ufService: UFService, private samuService: SamuService)
     { }
@@ -22,5 +29,8 @@ export class AppComponent implements OnInit {
     ngOnInit(): void {
         this.ufs = this.ufService.getAll();
         this.dados_da_samu = this.samuService.getAllMunicipiosAtendidosPorEstado();
+        this.uf = this.ufService.buscarUF(this.id);
+        this.dados = this.samuService.buscarSamu(this.id)
+        this.media = this.samuService.calcularMedia(this.id);
     }
 }
